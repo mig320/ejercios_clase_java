83f53dc3943e3c36673db53646438663ce1cd552
mig320 <pedraza.2001@hotmail.com>
2021-07-05 00:46:27 -0500

    file until exercise 5

--- src/main/java/miguel_projects/com/App.java
+++ src/main/java/miguel_projects/com/App.java
@@ -3,6 +3,10 @@ package miguel_projects.com;
 
 import java.util.Scanner;
 
+import java.math.*;
+
+
+
 /**
  * Hello world!
  *
@@ -47,6 +51,50 @@ public class App
     }
 
 
+    public static String convert_velocity(Double velocity){
+        var message = " ";
+        
+        var metters = 1000.0; // 1000 metters in a Km
+        var seconds = 3600.0; // 3600 seconds in a hour.
+        velocity  =  velocity * (metters / seconds);
+
+        message  = "the velocity in m/s is: " + velocity;
+
+
+        return  message;
+
+    }
+
+
+    public static boolean validacion(double cateto_1 , double cateto_2){
+
+        if (cateto_1 < 1  | cateto_2 < 1){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+    public static String hipotenusa(double cateto_1, double cateto_2){
+            var message =  " ";
+            var hipotenusa = 0.0;
+            
+            //elevar al cuadrado los catetos
+            cateto_1 = Math.pow(cateto_1, 2);
+            cateto_2 = Math.pow(cateto_2, 2);
+
+            // sacar raiz de la suma de los catetos
+            hipotenusa = Math.sqrt(cateto_1 + cateto_2);
+        
+        
+
+        return message = "la hipotenusa es: " + hipotenusa; 
+
+    }
+
+
+
 
     public static void main( String[] args )
     {
@@ -78,9 +126,31 @@ public class App
 
         System.out.println(valor_digitado(n, m));
 
+        /********************************************Ejercicio 4 ***************************************************/
+        System.out.println("digite la velocidad en Km/H");
+        var velocity =  valores.nextDouble();
 
-        
+        System.out.println(convert_velocity(velocity));
 
+        /********************************************** Ejercicio 5 *************************************************************/
+        var cateto_1 = 0.0;
+        var cateto_2 = 0.0;
+        var validacion = false;
+
+        while (validacion != true){
+            System.out.println("digite la longitud del primer cateto");
+            cateto_1  = valores.nextDouble();
+            System.out.println("digite la longitud del segundo cateto");
+            cateto_2 = valores.nextDouble();
+            validacion = validacion(cateto_1, cateto_2);
+            
+            if (validacion != true){
+
+                System.out.println("alguno de los valores ingresados es menor a 1");
+            }
+
+       }
+       System.out.println(hipotenusa(cateto_1, cateto_2));
         valores.close();
     }
 }
